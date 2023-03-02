28b8685b0dcd211cc018193fd2b9c0f64f177ccc
marcofloriano <marcofloriano@gmail.com>
2023-03-02 01:24:54 +0000

    Form de login adicionado

--- aulapress.php
+++ aulapress.php
@@ -72,7 +72,44 @@ function registration_form($username,$password,$email,$website,$first_name,$last
     ';
 }
 
-// Validate and sanitize the user input
+// HTML code of the login form
+function login_form() {
+    echo '
+    <style>
+    div {
+        margin-bottom:20px;
+    }
+    input[type=text], input[type=password] {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        box-sizing: border-box;
+        
+    }
+    input[type=submit] {
+        width: auto;
+        display: block;
+        margin: 0 auto;
+        padding: 6px 10px;
+    }
+    </style>
+    ';
+    echo '
+    <form action="' . $_SERVER['REQUEST_URI'] . '" method="post" >
+    <div>
+    <label for="username">Usuário <strong>*</strong></label>
+    <input type="text" name="username" value="' . (isset($_POST['username']) ? $username : null) . '">
+    </div>
+    <div>
+    <label for="password">Senha <strong>*</strong></label>
+    <input type="password" name="password" value="' . (isset($_POST['password']) ? $password : null) . '">
+    </div>
+    <input type="submit" name="submit" value="Acessar"/>
+    </form>
+    ';
+}
+
+// Validate and sanitize the user input at registration form
 function registration_validation($username,$password,$email,$website,$first_name,$last_name,$nickname,$bio)  {
     // Instantiate the WP_Error class and make the instance variable global so it can be access outside the scope of the function
     global $reg_errors;
@@ -122,6 +159,25 @@ function registration_validation($username,$password,$email,$website,$first_name
     }
 }
 
+// Validate and sanitize the user input at login form
+function login_validation($username, $password) {
+    global $reg_errors;
+    $reg_errors = new WP_Error;
+    // Check if any of the field is empty. If empty, we append the error message to the global WP_Error class
+    if(empty($username) || empty($password)) {
+        $reg_errors->add('field', 'Exitem campos obrigatórios não preenchidos.');    
+    }
+    // Loop through the errors in our WP_Error instance and display the individual error
+    if(is_wp_error($reg_errors)) {
+        foreach($reg_errors->get_error_messages() as $error) {   
+            echo '<div>';    
+            echo '<strong>Erro</strong>: ';    
+            echo $error . '<br/>';    
+            echo '</div>';
+        }
+    }
+}
+
 // Handles the user registration
 function complete_registration() {
     global $reg_errors, $username, $password, $email, $website, $first_name, $last_name, $nickname, $bio;
@@ -143,7 +199,36 @@ function complete_registration() {
 	}
 }
 
-// Function that puts all the functions we've created above into use
+// Handles the user login
+// function complete_login() {
+//     global $username, $password;
+//     if ( ! is_user_logged_in() ) {
+//         $userdata = array(
+//             'redirect' => admin_url(), // redirect to admin dashboard.
+//             'user_login'    => 	$username,
+//             'user_pass' 	=> 	$password,
+//             'remember' => true
+//         );
+//     wp_login_form( $userdata );
+//     }
+// }
+
+function login_wordpress($username, $password) {
+    global $username, $password;
+    $userdata = array();
+    $userdata['user_login'] = $username;
+    $userdata['user_password'] = $password;
+    $userdata['remember'] = true;
+    $user = wp_signon( $userdata, false );
+    if ( is_wp_error($user) ) {
+       echo $user->get_error_message();
+       die();
+    } else {
+        wp_set_auth_cookie( $user->ID, 1, is_ssl() );
+    }
+}
+
+// Function that puts all the registration functions we've created above into use
 function custom_registration_function() {
     if(isset($_POST['submit'])) {
         registration_validation(
@@ -156,7 +241,7 @@ function custom_registration_function() {
             $_POST['nickname'],
             $_POST['bio']
     	);
-        // sanitize user form input
+        // sanitize user registration form input
         global $username, $password, $email, $website, $first_name, $last_name, $nickname, $bio;
         $username	= 	sanitize_user($_POST['username']);
         $password 	= 	esc_attr($_POST['password']);
@@ -191,6 +276,32 @@ function custom_registration_function() {
 	);
 }
 
+// Function that puts all the login functions we've created above into use
+function custom_login_function() {
+    if(isset($_POST['submit'])) {
+        login_validation(
+            $_POST['username'],
+            $_POST['password']
+    	);
+        // sanitize user login form input
+        global $username, $password;
+        $username	= 	sanitize_user($_POST['username']);
+        $password 	= 	esc_attr($_POST['password']);
+		// call @function complete_login to redirect the user
+		// only when no WP_error is found
+        login_wordpress($username,$password);
+        wp_redirect(home_url());
+    }
+    elseif(is_user_logged_in() == true) {
+        echo '<div>';
+        echo 'Logado com sucesso! Para sair <a href="' . wp_logout_url(get_permalink()) . '">clique aqui.</a>';
+        echo '<div>';
+    }
+    else {
+        login_form();
+    }
+}
+
 // Register a new shortcode: [aulapress_custom_registration]
 add_shortcode( 'aulapress_custom_registration', 'custom_registration_shortcode' );
 // The callback function that will replace [book]
@@ -198,4 +309,13 @@ function custom_registration_shortcode() {
     ob_start();
     custom_registration_function();
     return ob_get_clean();
+}
+
+// Register a new shortcode: [aulapress_custom_login]
+add_shortcode( 'aulapress_custom_login', 'custom_login_shortcode' );
+// The callback function that will replace [book]
+function custom_login_shortcode() {
+    // ob_start();
+    custom_login_function();
+    // return ob_get_clean();
 }
