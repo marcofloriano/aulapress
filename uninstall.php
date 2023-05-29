1de454c6fbeae2a0c1d43c220113105490310fc5
Marco Floriano <marcofloriano@gmail.com>
2023-05-29 12:33:37 -0300

    Aulapress base plugin

--- uninstall.php
+++ uninstall.php
@@ -20,7 +20,8 @@ remove_role( 'aulapress_teacher' );
 remove_role( 'aulapress_student' );
 
 /**
- * Removes the options created by the plugin upon activation
+ * Removes the options created by the plugin upon activation 
+ * for users (public) and plugin (admin)
  *
  */
 delete_option( 'aulapress_plugin_public_options' );
@@ -30,13 +31,7 @@ delete_option( 'aulapress_plugin_admin_options' );
  * Remove Aulapress settings and clean the database
  *
  */
-register_uninstall_hook( __FILE__, 'aulapress_plugin_uninstall' );
-// Deregister Aulapress settings group and delete all options
-function aulapress_plugin_uninstall() {
-
-	// Clean de-registration of registered setting
-	unregister_setting( 'aulapress_plugin_options', 'aulapress_plugin_options' );
-
-	// Remove saved options from the database
-	delete_option( 'aulapress_plugin_options' );
-}
+// Clean de-registration of registered setting
+unregister_setting( 'aulapress_plugin_options', 'aulapress_plugin_options' );
+// Remove saved options from the database
+delete_option( 'aulapress_plugin_options' );
